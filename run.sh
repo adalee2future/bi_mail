a28ffb37b72db7a6f9724e1e1a4219a73d3f6303
ada.li <adalee2future@163.com>
2018-01-07 01:36:20 +0800

    fix exit code error

--- run.sh
+++ run.sh
@@ -12,11 +12,16 @@ fi
 if [[ ! -d reports/$report_id/data ]]
   then mkdir reports/$report_id/data
 fi
+
 log_file=reports/$report_id/log/`date +%Y-%m-%d_%H:%M:%S`.log
+echo $log_file
 touch $log_file
 ./send_report.sh $report_id >> $log_file 2>&1
 
-if [[ $? != 0 ]]
-  echo $log_file
-  then ./send_log.py $report_id $log_file
+res_code=$?
+echo send_report result: $res_code
+if [[ $res_code -eq 0 ]]
+  then echo
+else
+  ./send_log.py $report_id $log_file
 fi
