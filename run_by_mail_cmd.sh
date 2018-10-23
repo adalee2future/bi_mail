c6ae57b6c8a691675e443d2cda64e5d068524241
ada.li <adalee2future@163.com>
2018-10-23 18:06:20 +0800

    手动运行脚本报错发邮件给管理员

--- run_by_mail_cmd.sh
+++ run_by_mail_cmd.sh
@@ -11,3 +11,10 @@ echo $log_file
 touch $log_file
 ./run_by_mail_cmd.py >> $log_file 2>&1
 
+res_code=$?
+
+if [[ $res_code -eq 0 ]]
+  then echo
+else
+  python file_to_mail.py $log_file '手动运行报表脚本(run_by_mail_cmd.py) 错误!!!' '' $mail_monitor
+fi
