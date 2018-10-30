f9023a1a9722f6bf4793260f6fd9d40bc0461fe7
ada.li <adalee2future@163.com>
2018-10-30 10:48:49 +0800

    don't print multiple run message when first run

--- helper.py
+++ helper.py
@@ -6,12 +6,18 @@ def multiple_trials(wait_seconds=[0, 60, 120]):
         @wraps(func)
         def wrapper(*args, **kwargs):
             for idx, wait_second in enumerate(wait_seconds):
+                msg = ''
+                msg += ('wait %s seconds\n' % wait_second)
+                msg += ('multiple_trials %s: %s, %s, %s\n' % (idx + 1, func.__name__, args, kwargs))
+
                 time.sleep(wait_second)
                 try:
-                    print('multiple_trials %s: %s, %s, %s' % (idx + 1, func.__name__, args, kwargs))
-                    print('wait %s seconds' % wait_second)
+                    if idx > 0:
+                        print(msg)
                     return func(*args, **kwargs)
                 except Exception as e:
+                    if idx == 0:
+                        print(msg)
                     print('ERROR: %s\n' % e)
         return wrapper
     return _multiple_trials
