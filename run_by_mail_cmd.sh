2f22a4c4a7db30b0f15d8001893289fc4af4d7ef
ada.li <adalee2future@163.com>
2019-12-28 17:35:10 +0800

    force only one run_by_mail shell in a directory

--- run_by_mail_cmd.sh
+++ run_by_mail_cmd.sh
@@ -9,7 +9,11 @@ cd `dirname $0`
 log_file=log/`date +%Y-%m-%d`.log
 echo $log_file
 touch $log_file
-python run_by_mail_cmd.py >> $log_file 2>&1
+
+mail_monitor=`python -c "from helper import MAIL_MONITOR; print(MAIL_MONITOR)"`
+
+kill $(ps aux | grep run_by_mail_cmd.py | grep python | grep `pwd` | awk '{print $2}')
+python `pwd`/run_by_mail_cmd.py >> $log_file 2>&1
 
 res_code=$?
 
